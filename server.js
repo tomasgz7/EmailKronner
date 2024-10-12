0f4c95d954d894d23328e070bf3a9f6442a62cf5
tomasgz7 <tomasguzmancabj@hotmail.com>
2024-10-12 11:54:20 -0300

    cambios nuevos

--- server.js
+++ server.js
@@ -2,42 +2,48 @@ const express = require("express");
 const nodemailer = require("nodemailer");
 const bodyParser = require("body-parser");
 const cors = require("cors");
-
-require("dotenv").config();
+require("dotenv").config(); // Carga las variables de entorno desde el archivo .env
 
 const app = express();
 const port = 3000;
 
+// Configuración de CORS para permitir solicitudes desde el frontend
 app.use(cors());
+
+app.use(cors({ origin: "http://localhost:5500" })); // Cambia la URL al dominio de tu frontend si es diferente
 app.use(bodyParser.json());
 
+// Ruta para enviar el correo
 app.post("/send-email", async (req, res) => {
   const { name, email, message } = req.body;
 
+  // Configuración del transportador de Nodemailer
   let transporter = nodemailer.createTransport({
     service: "gmail", // Usamos el servicio de Gmail
     auth: {
-      user: "kronnersrl@gmail.com", // El correo de Gmail
-      pass: "neem iued rnxb vkfu", // La contraseña de tu correo Gmail
+      user: process.env.EMAIL_USER, // Correo desde las variables de entorno
+      pass: process.env.EMAIL_PASS, // Contraseña o contraseña de aplicación desde las variables de entorno
     },
     tls: {
       rejectUnauthorized: false, // Permitir certificados no verificados
     },
   });
 
+  // Configuración del correo a enviar
   let mailOptions = {
-    from: `<kronnersrl@gmail.com>`,
-    to: "hola@kronnerlogistica.com.ar",
-    subject: `Nuevo mensaje de ${name}`,
-    text: `Has recibido un nuevo mensaje de contacto.
-        Nombre: ${name}
-        Correo: ${email}
-        Mensaje: 
-        ${message}
+    from: `<${process.env.EMAIL_USER}>`, // Correo del remitente
+    to: "elocomy98@gmail.com", // Dirección de correo del destinatario
+    subject: `Nuevo mensaje de ${name}`, // Asunto del correo
+    text: `Has recibido un nuevo mensaje de contacto.\n
+        Nombre: ${name}\n
+        Correo: ${email}\n
+        Mensaje:\n
+        ${message}\n
     `,
   };
 
   try {
+    // Enviar el correo
     let info = await transporter.sendMail(mailOptions);
     console.log("Correo enviado: %s", info.messageId);
     res.status(200).send({ message: "Correo enviado con éxito" });
@@ -47,6 +53,7 @@ app.post("/send-email", async (req, res) => {
   }
 });
 
+// Iniciar el servidor
 app.listen(port, () => {
   console.log(
     `Servidor de envío de correos escuchando en http://localhost:${port}`
